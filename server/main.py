d6c05f4dcb06c91c712fa5dba98e78b1d422022d
Rowan Gordon <rowan@rowangordon.co.uk>
2022-10-05 20:54:30 +0100

    added sells to temporary database and requests

--- server/main.py
+++ server/main.py
@@ -4,6 +4,7 @@ from pydantic import BaseModel
 
 app = FastAPI()
 
+##### Data model for Buys #####
 class Buy(BaseModel):
   name: str
   ticker: str
@@ -11,6 +12,15 @@ class Buy(BaseModel):
   amount: float
   datetime: datetime
 
+##### Data model for Sells #####
+class Sell(BaseModel):
+  name: str
+  ticker: str
+  price_sold_for: float
+  amount: float
+  datetime: datetime
+
+##### Fake database to act as data until SQL server is setup #####
 fake_database = {
   "buys": {
     1: {
@@ -47,13 +57,19 @@ fake_database = {
 }
 
 ##### Get an buy by item_id #####
-@app.get('/user/investments/{item_id}')
-async def get_investment(item_id: int):
+@app.get('/user/buy/{item_id}')
+async def get_buy(item_id: int):
   return fake_database["buys"][item_id]
 
+##### Get an sell by item_id #####
+@app.get('/user/sell/{item_id}')
+async def get_sell(item_id: int):
+  return fake_database["sells"][item_id]
+
+
 ##### Create a new buy #####
-@app.post('/user/investments')
-async def create_investment(investment: Buy):
+@app.post('/user/buy')
+async def create_buy(investment: Buy):
   new_id = len(fake_database["buys"].keys()) + 1
   fake_database["buys"][new_id] = {
     "name": investment.name,
@@ -63,9 +79,22 @@ async def create_investment(investment: Buy):
   }
   return fake_database["buys"]
 
+##### Create a new sell #####
+@app.post('/user/sell')
+async def create_sell(investment: Sell):
+  new_id = len(fake_database["sells"].keys()) + 1
+  fake_database["sells"][new_id] = {
+    "name": investment.name,
+    "ticker": investment.ticker,
+    "price_bought_for": investment.price_sold_for,
+    "amount": investment.amount,
+  }
+  return fake_database["sells"]
+
+
 ##### Update a buy by item_id #####
-@app.put('/user/investments{item_id}')
-async def update_investment(item_id: int, investment: Buy):
+@app.put('/user/buy/{item_id}')
+async def update_buy(item_id: int, investment: Buy):
   fake_database["buys"][item_id] = {
     "name": investment.name,
     "ticker": investment.ticker,
@@ -74,8 +103,26 @@ async def update_investment(item_id: int, investment: Buy):
   }
   return fake_database["buys"]
 
+##### Update a sell by item_id #####
+@app.put('/user/sell/{item_id}')
+async def update_buy(item_id: int, investment: Sell):
+  fake_database["sells"][item_id] = {
+    "name": investment.name,
+    "ticker": investment.ticker,
+    "price_bought_for": investment.price_sold_for,
+    "amount": investment.amount,
+  }
+  return fake_database["sells"]
+
+
 ##### Delete a buy by item_id #####
-@app.delete('/user/investments/{item_id}')
-async def delete_investment(item_id: int):
+@app.delete('/user/buy/{item_id}')
+async def delete_buy(item_id: int):
   del fake_database["buys"][item_id]
   return fake_database["buys"]
+
+##### Delete a sell by item_id #####
+@app.delete('/user/sell/{item_id}')
+async def delete_buy(item_id: int):
+  del fake_database["sells"][item_id]
+  return fake_database["sells"]
