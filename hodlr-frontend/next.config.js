16f6f44b3e73da94fd3954698ed401dd14922afc
Rowan Gordon <rowan@rowangordon.co.uk>
2022-10-20 11:06:41 +0100

    added all rewrites

--- hodlr-frontend/next.config.js
+++ hodlr-frontend/next.config.js
@@ -8,8 +8,37 @@ const nextConfig = {
   async rewrites() {
     return [
       {
-        source: "/api",
-        destination: "https://hodlr-tracker.herokuapp.com/api",
+        source: "/api/users",
+        destination: "https://hodlr-tracker.herokuapp.com/api/users",
+      },
+      {
+        source: "/api/token",
+        destination: "https://hodlr-tracker.herokuapp.com/api/token",
+      },
+      {
+        source: "/api/users/me",
+        destination: "https://hodlr-tracker.herokuapp.com/api/users/me",
+      },
+      {
+        source: "/api/investment",
+        destination: "https://hodlr-tracker.herokuapp.com/api/investment",
+      },
+      {
+        source: "/api/coins_held",
+        destination: "https://hodlr-tracker.herokuapp.com/api/coins_held",
+      },
+      {
+        source: "/api/investments/:slug",
+        destination:
+          "https://hodlr-tracker.herokuapp.com/api/investments/:slug",
+      },
+      {
+        source: "/api/coins_held/:slug",
+        destination: "https://hodlr-tracker.herokuapp.com/api/coins_held/:slug",
+      },
+      {
+        source: "/api/prices",
+        destination: "https://hodlr-tracker.herokuapp.com/api/prices",
       },
     ];
   },
